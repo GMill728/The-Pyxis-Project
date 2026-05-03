51d4f14224506b005135284d2edb6055dd19bcf6
Gavv <mills.gavin1945@gmail.com>
2026-05-03 18:17:55 -0400

    disapearing map

--- Assets/Scripts/MinimapObjVisibility.cs
+++ Assets/Scripts/MinimapObjVisibility.cs
@@ -4,36 +4,20 @@ public class MinimapObjVisibility : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Collider room;
+    public GameObject minimapRoom;
 
     bool isActive = false;
     
     void Start()
     {
-        activateMap(false);
+       minimapRoom.SetActive(false);
     }
         
-
     // Update is called once per frame
     void Update(){}
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        
-        if (other.CompareTag("Player"))
-        {
-            activateMap(true);
-            isActive = true;
-        }
+        minimapRoom.SetActive(true);
     }
 
-    private void activateMap(bool active){
-        // foreach (Transform child in transform)
-        //         {
-        //             child.gameObject.SetActive(active);
-        //         }
-        Renderer[] renderers = GetComponentsInChildren<Renderer>();
-            foreach (Renderer r in renderers)
-            {
-                r.enabled = active;
-            }
-    }
 }
