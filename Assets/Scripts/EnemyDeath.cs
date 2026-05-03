ee069b41a69292b4605add9b28501c986ef359cb
William Grant/Fayeble <billwow601@gmail.com>
2026-05-03 18:02:17 -0400

    Deactivates entire enemy and makes them stay a bit

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyDeath : MonoBehaviour
 {
@@ -7,13 +8,18 @@ public class EnemyDeath : MonoBehaviour
         this.GetComponent<EnemyMovement>().enabled = false;
         this.GetComponent<Enemy_Attack>().enabled = false;
         this.GetComponent<CapsuleCollider>().enabled = false;
-        
+        this.GetComponent<NavMeshAgent>().enabled = false;
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
         //Play animation once here, no loop. Might set up timer for destruction later, idk
         Animator anim = GetComponent<Animator>();
         anim.SetTrigger("Die");
         gameObject.GetComponent<Billboard_Behavior>().enabled = false;
         
-        float deathLength = anim.GetCurrentAnimatorStateInfo(0).length;
+        float deathLength = anim.GetCurrentAnimatorStateInfo(0).length + 60;
         Destroy(gameObject, deathLength);
     }
 }
