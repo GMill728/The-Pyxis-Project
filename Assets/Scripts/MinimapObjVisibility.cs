cf6958a1d226e57f84415b6fe7d62d3378c56f91
Gavv <mills.gavin1945@gmail.com>
2026-05-03 18:02:39 -0400

    Updated empty prefab

--- Assets/Scripts/MinimapObjVisibility.cs
+++ Assets/Scripts/MinimapObjVisibility.cs
@@ -3,14 +3,37 @@ using UnityEngine;
 public class MinimapObjVisibility : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Collider room;
+
+    bool isActive = false;
+    
     void Start()
     {
-        
+        activateMap(false);
     }
+        
 
     // Update is called once per frame
-    void Update()
+    void Update(){}
+    private void OnTriggerEnter(Collider other)
     {
         
+        if (other.CompareTag("Player"))
+        {
+            activateMap(true);
+            isActive = true;
+        }
+    }
+
+    private void activateMap(bool active){
+        // foreach (Transform child in transform)
+        //         {
+        //             child.gameObject.SetActive(active);
+        //         }
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in renderers)
+            {
+                r.enabled = active;
+            }
     }
 }
