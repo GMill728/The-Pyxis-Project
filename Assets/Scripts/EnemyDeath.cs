f2579ad896fe112a15a6c0c6314d3bd197b1c717
Gavv <mills.gavin1945@gmail.com>
2026-05-03 17:51:25 -0400

    Kill and disable Billboarding

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -11,8 +11,9 @@ public class EnemyDeath : MonoBehaviour
         //Play animation once here, no loop. Might set up timer for destruction later, idk
         Animator anim = GetComponent<Animator>();
         anim.SetTrigger("Die");
+        gameObject.GetComponent<Billboard_Behavior>().enabled = false;
         
-        //float deathLength = anim.GetCurrentAnimatorStateInfo(0).length;
-        //Destroy(gameObject, deathLength);
+        float deathLength = anim.GetCurrentAnimatorStateInfo(0).length;
+        Destroy(gameObject, deathLength);
     }
 }
