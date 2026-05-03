e3de1b3b73aae020f22910df9e462ede0d6e489c
William Grant/Fayeble <billwow601@gmail.com>
2026-05-03 16:12:41 -0400

    Update SceneLoader.cs

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,24 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private string previousScene;
     public void LoadSceneByName(string sceneName)
     {
         if(sceneName == "MainMenu")
         {
             Time.timeScale = 1.0f;
         }
+        previousScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(sceneName);
     }
 
     public void LoadSceneByIndex(int sceneIndex)
     {
+        previousScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(sceneIndex);
     }
 
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        previousScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadPreviousScene()
+    {
+        previousScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(previousScene);
     }
 
     public void EndGame()
