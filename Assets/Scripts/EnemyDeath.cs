a6b609926db2217f9b1c1c5e29b09ea014a37679
Gavv <mills.gavin1945@gmail.com>
2026-05-03 17:29:44 -0400

    Working anim

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -7,6 +7,12 @@ public class EnemyDeath : MonoBehaviour
         this.GetComponent<EnemyMovement>().enabled = false;
         this.GetComponent<Enemy_Attack>().enabled = false;
         this.GetComponent<CapsuleCollider>().enabled = false;
+        
         //Play animation once here, no loop. Might set up timer for destruction later, idk
+        Animator anim = GetComponent<Animator>();
+        anim.SetTrigger("Die");
+        
+        //float deathLength = anim.GetCurrentAnimatorStateInfo(0).length;
+        //Destroy(gameObject, deathLength);
     }
 }
